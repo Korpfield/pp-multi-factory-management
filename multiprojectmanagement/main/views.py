1b18e824dceececf245ae8af0603a4793662cccd
Korpfield <riku.siimesvaara@gmail.com>
2018-04-21 00:09:58 +0300

    Add loop for generating cards

--- multiprojectmanagement/main/views.py
+++ multiprojectmanagement/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .mock_project import project_json
+import json
 
 # Main page
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'projects': project_json['projects']})
