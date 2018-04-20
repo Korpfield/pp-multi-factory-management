37d90724d65de26b07dd476bf56ad89cbfbfa8a8
Korpfield <riku.siimesvaara@gmail.com>
2018-04-21 00:13:56 +0300

    Capitalize project names

--- multiprojectmanagement/main/mock_project.py
+++ multiprojectmanagement/main/mock_project.py
@@ -1,7 +1,7 @@
 project_json={
     "projects" : [
-        {"name" : "project1" , "progress" : "100" , "dueDate" : "24.12.2017" , "issues" : "0"},
-        {"name" : "project2" , "progress" : "88" , "dueDate" : "01.01.2019" , "issues" : "1"},
-        {"name" : "project1" , "progress" : "33" , "dueDate" : "01.05.2020" ,"issues" : "5"},
+        {"name" : "Project 1" , "progress" : "100" , "dueDate" : "24.12.2017" , "issues" : "0"},
+        {"name" : "Project 2" , "progress" : "88" , "dueDate" : "01.01.2019" , "issues" : "1"},
+        {"name" : "Project 3" , "progress" : "33" , "dueDate" : "01.05.2020" ,"issues" : "5"},
     ]
 }
