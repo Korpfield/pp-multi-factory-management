924be990cbace751508a372bd9f0b511378a45ce
Korpfield <riku.siimesvaara@gmail.com>
2018-04-20 23:27:32 +0300

    Added some colons

--- multiprojectmanagement/main/mock_project.py
+++ multiprojectmanagement/main/mock_project.py
@@ -1,8 +1,7 @@
 project_json={
     "projects" : [
-        {"name" : "project1" , "progress" : "100" , "dueDate" : "24.12.2017" , "issues" : "0"}
-        {"name" : "project2" , "progress" : "88" , "dueDate" : "01.01.2019" , "issues" : "1"}
-        {"name" : "project1" , "progress" : "33" , "dueDate" : "01.05.2020" ,"issues" : "5"}
+        {"name" : "project1" , "progress" : "100" , "dueDate" : "24.12.2017" , "issues" : "0"},
+        {"name" : "project2" , "progress" : "88" , "dueDate" : "01.01.2019" , "issues" : "1"},
+        {"name" : "project1" , "progress" : "33" , "dueDate" : "01.05.2020" ,"issues" : "5"},
     ]
 }
-
